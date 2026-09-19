79648cc5a1d7bc920fcf94f2f181c1a91d30160f
Anmol Singh <anmolpreetsingh510@gmail.com>
2026-09-19 18:23:39 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (68.63%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.java
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -2,17 +2,13 @@ class Solution {
     public int getSum(int a, int b) {
 
 
-        System.out.println( Integer.toBinaryString(a));
-          System.out.println( Integer.toBinaryString(b));
-
-        while (b != 0) {
+        while(b != 0){
             int carry = (a & b) << 1;
 
             a = a ^ b;
 
             b = carry;
         }
-        return a;
-        
+        return a ;
     }
 }
